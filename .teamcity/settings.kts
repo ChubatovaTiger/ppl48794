78cef14a7dcfc3592aeefd40cf4a546e130de48a
Nastasia Chubatova <78346593+ChubatovaTiger@users.noreply.github.com>
2026-01-29 14:52:45 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,28 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
+import jetbrains.buildServer.configs.kotlin.pipelines.*
 
 version = "2025.11"
 
@@ -30,10 +9,10 @@ project {
 
     buildType(Project48794_B)
     buildType(Project48794_C)
-    buildType(Project48794_A)
+    pipeline(Project48794_A)
 }
 
-object Project48794_A : BuildType({
+object Project48794_A : Pipeline({
     id("A")
     name = "A"
 
@@ -41,20 +20,20 @@ object Project48794_A : BuildType({
         param("parA", "valA")
     }
 
-    vcs {
-        root(DslContext.settingsRoot)
+    repositories {
+        main(DslContext. settingsRoot)
     }
 
-    features {
-        perfmon {
-        }
-    }
 
     dependencies {
         snapshot(Project48794_B) {
         }
         snapshot(Project48794_C) {
         }
+    }
+    job {
+        id = "Job1"
+        name = "Job1"
     }
 })
 
